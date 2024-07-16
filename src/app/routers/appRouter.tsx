9cf0ca7dacfe4fb9f34ffac3f53f86efb341adbd
Владислав Попов <vladislavpopov.dev@gmail.com>
2024-07-16 12:59:16 +0300

    feat: create layout component and use it in appRouter

--- src/app/routers/appRouter.tsx
+++ src/app/routers/appRouter.tsx
@@ -1,11 +1,18 @@
 import { Home } from '@/pages/home'
+import { Layout } from '@/shared/ui'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 export const AppRouter = () => {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+      ],
     },
   ])
 
